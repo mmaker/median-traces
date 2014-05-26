1214c17ab3cc7d508f8ef1f59d05f96a587693e5
Michele Orrù <maker@python.it>
2014-05-26 15:54:35 +0200

    Some bash king-fu to reorder dataset generation in generate_dataset.bash.

--- generate_dataset.bash
+++ generate_dataset.bash
@@ -1,97 +1,98 @@
 #!/bin/bash
 
+# expecting UCIDv2 dataset.download from
+# <http://homepages.lboro.ac.uk/~cogs/datasets/ucid/data/ucid.v2.tar.gz>
 DATASET="$1"
 
+PARALLEL="parallel"
+
+DOMAIN="-colorspace gray"
+LUMINANCE_DOMAIN=""
+MEDIAN3="-median 3"
+MEDIAN5="-median 5"
+GAUSSIAN="-gaussian-blur 0.5"
+AVERAGE="-define convolve:scale=! -morphology Convolve square:3"
+COMPRESS="-format jpg -quality 70%"
+CROP="-gravity center -crop 128x128+0+0"
+CROP_64="-gravity center -crop 64x64+0+0"
+CROP_256="-gravity center -crop 256x256+0+0"
+RESIZE="-resize 110% -resize 128x128"
+
+
 mkdir -p dataset/ucid
-
 tar xvf $DATASET -C dataset/ucid
-
 cd dataset/ucid
-#### 128x128 images #####
+
+PATTERN='*.tif'
+make_dataset()
+{
+    dataset_dir="../$1"
+    pattern="$2"
+    args="${@:2:$#}"
+    [ -d "$dataset_dir" ] || mkdir "$dataset_dir"
+    find . -name "$PATTERN" | $PARALLEL convert {} $args "$dataset_dir/{.}.jpg"
+}
 
 echo 'original image dataset'
-mkdir ../ori
-find . -name '*.tif' -exec convert {} -gravity center -crop 128x128+0+0 -colorspace Gray ../ori/{} \;
-cd ../ori
-
+make_dataset ori $CROP $DOMAIN $COMPRESS
 echo '3x3 median filtering'
-mkdir ../mf3
-find . -name '*.tif' -exec convert {} -median 3 ../mf3/{} \;
-
+make_dataset mf3 $CROP $DOMAIN $MEDIAN3 $COMPRESS
 echo '5x5 median filtering'
-mkdir ../mf5
-find . -name '*.tif' -exec convert {} -median 5 ../mf5/{} \;
-
+make_dataset mf5 $CROP $DOMAIN $MEDIAN5 $COMPRESS
 echo 'gaussian blur with standard deviation 0.5'
-mkdir ../gau
-find . -name '*.tif' -exec convert {} -gaussian-blur 0.5 ../gau/{} \;
-
+make_dataset gau $CROP $DOMAIN $GAUSSIAN $COMPRESS
 echo 'rescaling with the scaling factor of 1.1'
-mkdir ../res
-find . -name '*.tif' -exec convert {} -resize 110% -resize 128x128 ../res/{} \;
-
+make_dataset res $CROP $DOMAIN $RESIZE $COMPRESS
 echo '3x3 average filtering'
-mkdir ../ave
-find . -name '*.tif' -exec convert {} -define convolve:scale=! -morphology Convolve square:3 ../ave/{} \;
-
-echo '3x3 and 5x5 median filtering'
-mkdir ../mf35
-find . -name '*[0-8][0-9][02468].tif' -exec convert {} -median 3 ../mf35/{} \;
-find . -name '*[0-8][0-9][13579].tif' -exec convert {} -median 5 ../mf35/{} \;
-
-echo 'mixed'
+make_dataset ave $CROP $DOMAIN $AVERAGE $COMPRESS
+# mixed datasets
 cd ..
+echo '3x3 and 5x5 median filtering'
+mkdir mf35
+cp mf3/*[02468].jpg mf35/
+cp mf5/*[13579].jpg mf35/
+echo 'all!'
 mkdir all
-cp ori/ucid00[0-1]*.tif all/
-cp res/ucid00[2-3]*.tif all/
-cp gau/ucid00[4-5]*.tif all/
-cp ave/ucid00[6-7]*.tif all/
-cd all
+cp ori/ucid00[0-1]*.jpg all/
+cp res/ucid00[2-3]*.jpg all/
+cp gau/ucid00[4-5]*.jpg all/
+cp ave/ucid00[6-7]*.jpg all/
+cd ucid
 
-
-cd ../ucid
 ##### 64x64 images ####
-
 echo 'original image dataset 64x64'
-mkdir ../ori-64
-find . -name '*.tif' -exec convert {} -gravity center -crop 64x64+0+0 -colorspace Gray ../ori-64/{} \;
-cd ../ori-64/
-
+make_dataset ori-64 $CROP_64 $DOMAIN $COMPRESS
 echo '3x3 and 5x5 median filtering'
-mkdir ../mf35-64
-find . -name '*[0-7][0-9][02468].tif' -exec convert {} -median 3 ../mf35-64/{} \;
-find . -name '*[0-7][0-9][13579].tif' -exec convert {} -median 5 ../mf35-64/{} \;
+PATTERN='*[0-7][0-9][02468].tif'
+make_dataset mf35-64 $CROP_64 $DOMAIN $MEDIAN3 $COMPRESS
+PATTERN='*[0-7][0-9][13579].tif'
+make_dataset mf35-64 $CROP_64 $DOMAIN $MEDIAN5 $COMPRESS
+echo 'all-64'
+PATTERN='ucid00[0-1]*.tif'
+make_dataset all-64 $CROP_64 $DOMAIN $COMPRESS
+PATTERN='ucid00[2-3]*.tif'
+make_dataset all-64 $CROP_64 $DOMAIN $AVERAGE $COMPRESS
+PATTERN='ucid00[4-5]*.tif'
+make_dataset all-64 $CROP_64 $DOMAIN $GAUSSIAN $COMPRESS
+PATTERN='ucid00[6-7].tif'
+make_dataset all-64 $CROP_64 $DOMAIN $RESIZE $COMPRESS
+PATTERN='*.tif'
 
-echo 'mixed'
-mkdir ../all-64
-find . -name 'ucid00[0-1]*.tif' -exec convert {} ../all-64/{} \;
-find . -name 'ucid00[2-3]*.tif' -exec convert {} -define convolve:scale=! -morphology Convolve square:3 ../all-64/{} \;
-find . -name 'ucid00[4-5]*.tif' -exec convert {} -resize 110% -resize 128x128 ../all-64/{} \;
-find . -name 'ucid00[6-7].tif' -exec convert {} -gaussian-blur 0.5 ../all-64/{} \;
-
-
-cd ../ucid
-##### 256x256 images ####
-
+# ##### 256x256 images ####
 echo 'original image dataset 256x256'
-mkdir ../ori-256
-find . -name '*.tif' -exec convert {} -gravity center -crop 256x256+0+0 -colorspace Gray ../ori-256/{} \;
-cd ../ori-256
-
+make_dataset ori-256  $CROP_256 $DOMAIN $COMPRESS
 echo '3x3 and 5x5 median filtering'
-mkdir ../mf35-256
-find . -name '*[0-7][0-9][02468].tif' -exec convert {} -median 3 ../mf35-256/{} \;
-find . -name '*[0-7][0-9][13579].tif' -exec convert {} -median 5 ../mf35-256/{} \;
-
+PATTERN='*[0-7][0-9][02468].tif'
+make_dataset mf35-256 $CROP_256 $DOMAIN $MEDIAN3 $COMPRESS
+PATTERN='*[0-7][0-9][13579].tif'
+make_dataset mf35-256 $CROP_256 $DOMAIN $MEDIAN5 $COMPRESS
 echo 'mixed'
-mkdir ../all-256
-find . -name 'ucid00[0-1]*.tif' -exec convert {} ../all-256/{} \;
-find . -name 'ucid00[2-3]*.tif' -exec convert {} -define convolve:scale=! -morphology Convolve square:3 ../all-256/{} \;
-find . -name 'ucid00[4-5]*.tif' -exec convert {} -resize 110% -resize 128x128 ../all-256/{} \;
-find . -name 'ucid00[6-7]*.tif' -exec convert {} -gaussian-blur 0.5 ../all-256/{} \;
-
-cd ..
-#### JPEG compression ####
-find . -name '*.tif' -exec convert {} -quality 70% {}.jpeg \;
-rename 's/.tif.jpeg/.jpeg/' */*
-rm */*.tif
+PATTERN='ucid00[0-1]*.tif'
+make_dataset all-256 $CROP_256 $DOMAIN $COMPRESS
+PATTERN='ucid00[2-3]*.tif'
+make_dataset all-256 $CROP_256 $DOMAIN $AVERAGE $COMPRESS
+PATTERN='ucid00[4-5]*.tif'
+make_dataset all-256 $CROP_256 $DOMAIN $GAUSSIAN $COMPRESS
+PATTERN='ucid00[6-7]*.tif'
+make_dataset all-256 $CROP_256 $DOMAIN $RESIZE $COMPRESS
+PATTERN='*.tif'
