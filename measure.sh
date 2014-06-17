2619173bdd41c7a3305050b33b5dac7e2e5b35f4
Michele Orrù <maker@python.it>
2014-06-17 19:10:14 +0200

    Formatting for measure.sh.

--- measure.sh
+++ measure.sh
@@ -4,7 +4,7 @@ DPATH="$1"
 sets=('mf3' 'mf5' 'mf35' 'mf35-64' 'mf35-256')
 thresholds=$(seq 1 0.5 10)
 TEST="python median_traces.py test --path=$DPATH"
-MEASURE="python median_traces.py ssim"
+MEASURE="python median_traces.py measure"
 
 for s in $sets
 do
@@ -12,6 +12,7 @@ do
     do
 	antidir="$DPATH/anti-testcases/$s-$t/"
 	mfdir="$DPATH/$s"
-	$MEASURE $mfdir $antidir
+	printf "ssim $s $t " && $MEASURE ssim $mfdir $antidir
+    printf "psnr $s $t " && $MEASURE psnr $mfdir $antidir
     done
 done
