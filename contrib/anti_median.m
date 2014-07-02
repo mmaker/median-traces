d19c4d9e2673e888c27884b016b48603366dad84
Michele Orrù <maker@tumbolandia.net>
2014-07-02 16:48:22 +0200

    Remove fucking single

--- contrib/anti_median.m
+++ contrib/anti_median.m
@@ -1,7 +1,7 @@
 function anti_median(pattern, source, dest, s, t, a, b)
     files = dir(sprintf('%s/%s', source, pattern));
     for file = files'
-      I = single(imread(sprintf('%s/%s', source, file.name)));
+      I = imread(sprintf('%s/%s', source, file.name));
       W = impulsenoiseBlock(I, str2num(s), str2num(t), str2num(a), str2num(b));
       imwrite(W, sprintf('%s/%s', dest, file.name));
     end
